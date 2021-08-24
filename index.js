b6ddafea1d8b8029624ea3e256b84cc515a8da35
kavinnashi <kavinnashi@gmail.com>
2021-08-24 11:49:31 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let jsonData = {};
+function download(content, fileName, contentType) {
+  var a = document.createElement('a');
+  var file = new Blob([content], { type: contentType });
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
+download(JSON.stringify(jsonData), 'json.txt', 'text/plain');
